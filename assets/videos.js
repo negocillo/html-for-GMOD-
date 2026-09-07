0a79ee4694c2996ebe40f6f9faf266adc7609008
negocillo <112661658+negocillo@users.noreply.github.com>
2026-09-07 14:25:39 -0300

    Update video URLs to embed format and fix subtitle

--- assets/videos.js
+++ assets/videos.js
@@ -1,19 +1,19 @@
 window.LOADSCREEN_VIDEOS = [
-  "https://youtu.be/DRvPIIwleXo",
-  "https://youtu.be/gTXSJBXaSTw",
-  "https://youtu.be/cA7isqKYP1U",
-  "https://youtu.be/HEXb9OjGN_Y",
-  "https://youtu.be/HEXb9OjGN_Y",
-  "https://youtu.be/Xmgob8J38nQ",
-  "https://youtu.be/OGXx8P7wJ4w",
-  "https://youtu.be/1cTV_7X-MJw"
+  "https://www.youtube.com/embed/DRvPIIwleXo",
+  "https://www.youtube.com/embed/gTXSJBXaSTw",
+  "https://www.youtube.com/embed/cA7isqKYP1U",
+  "https://www.youtube.com/embed/HEXb9OjGN_Y",
+  "https://www.youtube.com/embed/Xmgob8J38nQ",
+  "https://www.youtube.com/embed/OGXx8P7wJ4w",
+  "https://www.youtube.com/embed/1cTV_7X-MJw"
 ];
 
 window.LOADSCREEN_SERVER = {
   name: "S&Box By Cilo LFS/LVS/NPC's muito +",
-  subtitle: "ESTAMOS EM REVOLRMA DESCULA O TRANSTORNO...",
+  subtitle: "ESTAMOS EM REFORMA DESCULPA O TRANSTORNO...",
   adminsSource: "",
   adminsRefreshMs: 30000,
+
   admins: [
     {
       name: "CILO - SC",
